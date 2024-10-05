ba96de9fb1d362f81d92427565151f7bad7215ce
niceProg <wisnu.yudhanta@gmail.com>
2024-10-06 04:42:18 +0700

    canvas3d & camera

---
+++ src/canvas/Shirt.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { useFrame } from "@react-three/fiber";
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
+
+import state from "../store";
+
+const Shirt = () => {
+     const snap = useSnapshot(state);
+     const { nodes, materials } = useGLTF("./shirt_baked.glb");
+
+     // Load textures
+     const logoTexture = useTexture(snap.logoDecal);
+     const fullTexture = useTexture(snap.fullDecal);
+
+     // Set anisotropy value directly to the textures (if available)
+     if (logoTexture) logoTexture.anisotropy = 16;
+
+     // Update material color using easing function
+     useFrame((state, delta) => {
+          if (materials.lambert1) {
+               easing.dampC(materials.lambert1.color, snap.color, 0.25, delta);
+          }
+     });
+
+     const stateString = JSON.stringify(snap);
+
+     return (
+          <group key={stateString}>
+               <mesh castShadow geometry={nodes.T_Shirt_male.geometry} material={materials.lambert1} material-roughness={1} dispose={null}>
+                    {/* Apply full texture if applicable */}
+                    {snap.isFullTexture && (
+                         <Decal
+                              position={[0, 0, 0]}
+                              rotation={[0, 0, 0]}
+                              scale={1}
+                              map={fullTexture} // No need to set anisotropy here
+                         />
+                    )}
+
+                    {/* Apply logo texture with anisotropy 16 */}
+                    {snap.isLogoTexture && <Decal position={[0, 0.04, 0.15]} rotation={[0, 0, 0]} scale={0.15} map={logoTexture} depthTest={false} depthWrite={true} />}
+               </mesh>
+          </group>
+     );
+};
+
+export default Shirt;
--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-const Canvas = () => {
-     return <div>Canvas</div>;
+import Shirt from "./Shirt";
+import CameraRig from "./CameraRig";
+import Backdrop from "./Backdrop";
+
+const CanvasModel = () => {
+     return (
+          <Canvas>
+               <ambientLight intensity={0.5}></ambientLight>
+               <Environment preset="city"></Environment>
+               <CameraRig>
+                    {/* <Backdrop></Backdrop> */}
+                    <Center>
+                         <Shirt></Shirt>
+                    </Center>
+               </CameraRig>
+          </Canvas>
+     );
 };
 
-export default Canvas;
+export default CanvasModel;
